e6f5e7365bedbd12bd00032ee40fc717ee391839
utkarsh3696 <karaleutkarsh3696@gmail.com>
2025-01-12 05:43:20 +0530

    [RM-2] Adding Templates UI and Dashboard Page UI
    - Designed dashboard page.
    - Designed UI login page.
    - Created separate components for all topbar, sidebar and footer.
    - Created Style.css file to use customize classes.
    - Updated SecurityConfig.java file to get login.html
    - Added StaticResourceConfig.java file for static resources.

---
+++ src/main/java/com/jiinfotech/restomanager/utils/StaticResourceConfig.java
@@ -0,0 +1,18 @@
+package com.jiinfotech.restomanager.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**", "static/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+}
