1007688f7cff3eb6d8b9eb90ea8becc09c6dd359
Utkarsh Karale <karaleutkarsh3696@gmail.com>
2025-01-26 16:43:52 +0530

    [RM-18] Order Place and Bill creation

--- src/test/java/com/jiinfotect/resto_manager/RestoManagerApplicationTests.java
+++ src/test/java/com/jiinfotect/resto_manager/RestoManagerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.jiinfotect.resto_manager;
 
+import com.jiinfotech.restomanager.RestoManagerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RestoManagerApplication.class)
 class RestoManagerApplicationTests {
 
 	@Test
